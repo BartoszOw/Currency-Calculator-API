2b230b3fcde4ef87aeceee9743fa5580d2cca9b9
BartoszOw <owbartosz.pl@gmail.com>
2024-03-06 00:52:56 +0100

    Site Changing

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
+from flask import Flask, render_template
 import requests
 import csv
 
+app = Flask(__name__)
 
 response = requests.get('https://api.nbp.pl/api/exchangerates/tables/C?format=json')
 data = response.json()
 data_rates = data.pop()
 rates = data_rates['rates']
-
-
-
 
 with open('data.csv','w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=['currency', 'code', 'bid', 'ask'])
@@ -17,3 +16,17 @@ with open('data.csv','w', newline='') as csvfile:
     for i in list(rates):
         writer.writerow({'currency': i['currency'], 'code': i['code'], 'bid': i['bid'], 'ask': i['ask']})
         
+
+
+@app.route('/', methods=['GET','POST'])
+def select():
+    return render_template('select.html')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
